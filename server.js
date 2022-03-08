dfc1827b440633850feaf1bb516abe3b4950d2fd
charisTheo <dev@charistheo.io>
2022-03-08 16:59:11 +0000

    Update build command for Heroku.

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+const dotenv = require('dotenv')
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config()
+}
 
 const path = require('path');
 const express = require('express')
