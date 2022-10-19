436bf79b45b6693090f7c6ecb0431ad520a6c795
Maor Rocky <maor@365scores.com>
2022-10-19 14:52:55 +0300

    saving changes.

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const app = express()
 const port = process.env.PORT || 8080
 app.use(bodyParser.json())
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   const publicPath = path.join(__dirname, "./web/build");
   app.use(express.static(publicPath))
   app.get('/profanity', (req, res) => res.sendFile(path.join(publicPath, '/index.html')))
   app.get('/login', (req, res) => res.sendFile(path.join(publicPath, '/index.html')))
   app.get('/', (req, res) => res.sendFile(path.join(publicPath, '/index.html')))
-// }
+}
 
 routes(app)
 
